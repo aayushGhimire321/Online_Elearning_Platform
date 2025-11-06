6950a16d6204266971a0eadd1c8d29a3025570cb
aayushGhimire321 <aghimire491@gmail.com>
2025-11-06 22:25:38 +0545

    adding sign-in & sign-up component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h2>Welcome to the Online Learning Platform</h2>
+      <Button>Hellow world</Button>
     </div>
   );
 }
